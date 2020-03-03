035208d30ce2925ea00df07bc36197e84edd5802
Gurmukh Kharod <gurmukhkharod@hotmail.com>
2020-03-02 16:59:38 -0800

    Initial commit

---
+++ app/src/main/java/com/example/gittutorials/Main2ActivityGit.java
@@ -0,0 +1,14 @@
+package com.example.gittutorials;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2ActivityGit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2_git);
+    }
+}
---
+++ app/src/main/java/com/example/gittutorials/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.gittutorials;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.btnClick);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Main2ActivityGit.class);
+                startActivity(i);
+            }
+        });
+    }
+}
